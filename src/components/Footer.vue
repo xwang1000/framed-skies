a0286e74800c59ecdb8545bd3e6454ba61276b82
xw <23066125+xwang1000@users.noreply.github.com>
2019-05-08 01:12:53 -1000

    Switch contact to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="footer">
-    <p>© 2019 by Sheng Chenni</p>
+    <p class="footer__copyright">© 2019 by Sheng Chenni</p>
+    <div class="footer__contact">
+      <span>
+        <i class="fab fa-instagram"></i>
+        <a href="https://www.instagram.com/sheng.jewellery/">sheng.jewellery</a>
+      </span>
+      <span>
+        <i class="fas fa-at"></i>
+        <a href="mailto:shengchenni2017@gmail.com">shengchenni2017@gmail.com</a>
+      </span>
+      <span>
+        <i class="fas fa-phone"></i>
+        <a href="tel:+4407514116528">+44 (0) 7514116528</a>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -9,15 +23,33 @@
   border-top: 1px solid #eee;
   position: absolute;
   width: 100%;
-  padding: .9rem 1rem;
-  height: 3rem;
-  box-sizing: border-box;
+  height: 4rem;
+  background-color: white;
   bottom: 0;
 }
 
-.footer p {
-  text-align: right;
+.footer__copyright {
+  opacity: 0.4;
+  margin-bottom: .3rem;
+}
+
+.footer__contact {
+  padding-bottom: 1rem;
+}
+
+.footer__contact span {
+  display: inline-block;
   opacity: .7;
-  margin: 0;
+  margin: .5rem 0;
+}
+
+i {
+  margin-left: 1.2rem;
+  margin-right: 0.3rem;
+  opacity: .8;
+}
+
+a {
+  color: inherit;
 }
 </style>
