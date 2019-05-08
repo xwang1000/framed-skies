94e42634391968d074ea110474456b6cbd45a9db
xw <23066125+xwang1000@users.noreply.github.com>
2019-05-08 10:07:00 -1000

    Implement options panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,40 @@
 <template>
   <div>
     <div class="options">
-      <span v-for="category in pictureCategories"
+      <span
+        v-for="category in pictureCategories"
         :key="category"
-      >
-        {{category}}
-      </span>
+        :class="{ disabledCategory: category !== selectedCategory }"
+      >{{category}}</span>
     </div>
     <div class="gallery">
-      <img 
-        v-for="picture in pictures" 
+      <img
+        v-for="picture in pictures"
         v-show="picture.category === selectedCategory"
-        :key="picture.name" 
-        :src="`${publicPath + picture.name}`" 
+        :key="picture.name"
+        :src="`${publicPath + picture.name}`"
         :alt="picture.name"
       >
     </div>
-
   </div>
-
-
 </template>
 
 <script>
-import pictures from '@/db.json'
+import pictures from "@/db.json";
 export default {
-  data () {
+  data() {
     return {
       publicPath: process.env.BASE_URL,
       pictures,
-      selectedCategory: 'untitled-moods'
-    }
+      selectedCategory: "untitled-moods"
+    };
   },
   computed: {
-    pictureCategories () {
-      return [...new Set(this.pictures.map(picture => picture.category))]
+    pictureCategories() {
+      return [...new Set(this.pictures.map(picture => picture.category))];
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -47,12 +44,21 @@ export default {
 
 .options span {
   display: inline-block;
-  border: 1px solid rgb(154, 186, 229);
-  color: rgb(154, 186, 229);
-  border-radius: 8px;
+  border-radius: 5px;
   margin: 0 5px;
   margin-bottom: 5px;
-  padding: 3px 4px;
+  padding: 3px 5px;
+}
+
+.options span:not(.disabledCategory) {
+  border: 1px solid rgb(154, 186, 229);
+  color: rgb(154, 186, 229);
+}
+
+.disabledCategory {
+  color: grey;
+  opacity: .5;
+  border: 0.5px solid grey;
 }
 
 .gallery img {
